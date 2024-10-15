4eaa5acc7a7074ce308d96c5369dee7bc2970ec8
fjd2004711 <fjd2004711@outlook.com>
2024-10-15 08:39:58 +0800

    bug fix

--- ruijie_nxu.sh
+++ ruijie_nxu.sh
@@ -18,14 +18,15 @@ log_file="/var/log/ruijie_nxu.log"
 network_status=""
 
 function check_connection() {
-    captiveReturnCode=$(curl -s -I -m 10 -o /dev/null -s -w %{http_code} http://www.baidu.com)
+    # 尝试访问宁夏大学图书馆
+    captiveReturnCode=$(curl -s -I -m 10 -o /dev/null -s -w %{http_code} https://metalib.nxu.edu.cn/space/index)
     if [ "${captiveReturnCode}" = "200" ]; then
-        echo "网络在线。"
+        echo "网络在线且已认证，校内资源访问正常。"
         network_status="online"
         return 0
     else
-        echo "网络离线，尝试重连。"
-        network_status="offline"
+        echo "网络离线或未认证。"
+        network_status="offline_or_pending_auth"
         return 1
     fi
 }
@@ -57,22 +58,18 @@ function connect() {
     encoded_service=""
 
     if [ "${service}" = "chinamobile" ]; then
-        echo "使用中国移动作为互联网服务提供商。" | tee -a "$log_file"
         encoded_service="${chinamobile}"
     fi
 
     if [ "${service}" = "chinanet" ]; then
-        echo "使用中国电信作为互联网服务提供商。" | tee -a "$log_file"
         encoded_service="${chinanet}"
     fi
 
     if [ "${service}" = "chinaunicom" ]; then
-        echo "使用中国联通作为互联网服务提供商。" | tee -a "$log_file"
         encoded_service="${chinaunicom}"
     fi
 
     if [ -z "${encoded_service}" ]; then
-        echo "使用校园网作为互联网服务提供商。" | tee -a "$log_file"
         encoded_service="${campus}"
     fi
 
@@ -83,7 +80,8 @@ function connect() {
     queryString="${queryString//&/%2526}"
     queryString="${queryString//=/%253D}"
 
-    if [ -n "${loginURL}" ]; then
+    if [ -n "${loginURL}" ];
+    then
         authResult=$(curl -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36" -e "${loginPageURL}" -b "EPORTAL_COOKIE_USERNAME=; EPORTAL_COOKIE_PASSWORD=; EPORTAL_COOKIE_SERVER=; EPORTAL_COOKIE_SERVER_NAME=; EPORTAL_AUTO_LAND=; EPORTAL_USER_GROUP=; EPORTAL_COOKIE_OPERATORPWD=;" -d "userId=${username}&password=${password}&service=${encoded_service}&queryString=${queryString}&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" "${loginURL}")
         echo "认证结果：$authResult" | tee -a "$log_file"
     fi
@@ -94,14 +92,21 @@ if [ "${action}" = "logout" ]; then
 else
     retry_count=0
     wait_time=60
-
     while true; do
         if check_connection; then
-            echo "网络状态为在线，等待下一次检测。"
-            # 如果网络恢复，重置重试次数和等待时间
-            retry_count=0
-            wait_time=5
-            sleep 5
+            case "${network_status}" in
+                "online")
+                    echo "网络状态为在线，等待下一次检测。"
+                    # 如果网络恢复，重置重试次数和等待时间
+                    retry_count=0
+                    wait_time=5
+                    sleep 5
+                    ;;
+                "offline_or_pending_auth")
+                    echo "网络离线或未认证，尝试进行认证。"
+                    connect  # 调用认证函数进行认证尝试
+                    ;;
+            esac
         else
             echo "网络状态为离线，尝试重连。"
             connect
